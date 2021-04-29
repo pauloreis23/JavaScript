c0ffec47a510b6fa2bfcc471241250ee97173d8c
Paulo Ricardo <paulo230301@hotmail.com>
2021-04-29 19:17:07 -0300

    Update comentarios.js

--- comentarios.js
+++ comentarios.js
@@ -4,6 +4,64 @@
 
 o codigo abaixo escreve no console uma mensagem
 
+console.log("Bem vindo ao JavaScript!!")
+
 */
 
-console.log("Bem vindo ao JavaScript!!")
+/* 
+    String
+
+    * cadeira de caracteres
+
+    "" // aspas duplas
+    '' // aspas simples
+    `` // template strings (permite multiplas linhas)
+
+    console.log("Java")
+
+*/
+
+/*
+    Numeros
+
+    NaN // Not a Number
+    Infinity // infinito
+
+*/
+
+/*
+    * undefined
+
+    * null
+        * objeto que não possui nada dentro
+        * diferente de undefined
+
+*/
+
+/*
+    Objeto
+        * Propriedades / Atributos
+        * Funcionalidades / Métodos
+
+    {propriedade: "valor"}
+
+    console.log({
+        name: Paulo,
+        idade 20,
+    })
+
+*/
+
+
+/*
+    Array
+        * vetor
+
+    console.log([
+        "ovos",
+        "leite",
+        2,
+        3
+    ])
+
+*/
